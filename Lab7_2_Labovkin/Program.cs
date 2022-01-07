167c27ca4ca3dab18a8be9ee68e7c34a7dfbf0c7
Дмитрий Лабовкин <labovkindi@mail.ru>
2022-01-07 17:36:50 +0300

    Добавьте файлы проекта.

---
+++ Lab7_2_Labovkin/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7_2_Labovkin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра a = {0} см");
+            int a = Convert.ToInt32(Console.ReadLine());
+            double S, V;
+            Cube(a, out S, out V);
+            Console.WriteLine("Площадь куба равна S ={0} кв.см",S);
+            Console.WriteLine("Объем куба равен V = {0} куб.см",V);
+            Console.ReadKey();
+        }
+        static void Cube(int a, out double S, out double V)
+        {
+            V = 6 * a * a;
+            S = a * a * a;
+        }
+    }
+}
